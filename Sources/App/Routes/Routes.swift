b3a5e72217bec8efc38ca35c2512cb029d0648d1
Alberto Scampini <Alberto.Scampini@nestle.com>
2017-10-11 15:38:56 +0200

    Clean up routes

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -6,32 +6,7 @@ extension Droplet {
     
     func setupRoutes() throws {
 
-        let pc = PeopleController()
         let uc = UsersController()
-
-        let tokenMiddleware = TokenAuthenticationMiddleware(MyUser.self)
-        /// use this route group for protected routes
-        let authed = self.grouped(tokenMiddleware)
-
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-
-        try resource("posts", PostController.self)
 
         // MARK: - Authentications
 
@@ -43,10 +18,6 @@ extension Droplet {
         }
 
         // MARK: - PeopleController handlers
-
-        get("show", handler: pc.showPeople)
-
-        post("add", handler: pc.addPerson)
 
         post("signIn", handler: uc.signIn)
 
